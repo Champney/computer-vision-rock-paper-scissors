7d6e5583b8714d44f84c339963a59f5215ca4eb9
Champney <davidjchampney@gmail.com>
2023-07-15 18:29:22 +0100

    Packs functions into a class and updates README inc images

--- README.md
+++ README.md
@@ -35,10 +35,40 @@ The next thing I needed was a Python file containing the logic for the game, thi
 
 ![Alt text](image-6.png)
 
-In the code, I imported the 'random' module so that I could use the 'choice()' method for the computer's decision, and used the 'input()' method for the user's decision (making sure to also use the 'capitalize()' method to standardize the user input)
+In the code, I imported the 'random' module so that I could use the 'choice()' method for the computer's decision, and used the 'input()' method for the user's decision.
 I created separate functions for computer and user input, as well as a third function called 'get_winner()' containing all of the game logic and passing two arguments; 'computer_choice' and 'user_choice'.
 Now, when running the code, when the two 'get_x_choice()' functions are passed as arguments to the 'get_winner()' function, the game runs as expected.
 As the three functions are all required to play the game, it made sense to pack them all into a function called 'play()'.
 
+## Milestone 5
+# Implementing the rules into the game
+At this stage I had all of the materials for a functioning game, all I needed to do was make them compatible with each other and debug the finished program.
+The bulk of the work, of course, was around the user input function, since this was the most complex as it is taking webcam input, in the place of a simple list combined with the random() method.
 
+![Alt text](image-7.png)
 
+As the webcam output assigned to the variable 'prediction' was displayed as a list of probabilities, outputting the correct gesture in string format was simply a matter of assigning the corresponding string to the 'x' variable when the camera model certainty was over a particular threshold(in this case I found around 75% to be the most successful).
+I achieved this using if/elif statements.
+
+One milestone requirement at this stage was to include a basic countdown for the user to indicate when they should make their gesture, I wrote this code as a 'while' loop immediately prior to the 'while' loop that captured the user input, avoiding using the 'sleep()' function as it could interfere with the camera user input.
+
+Another of the milestone requirements was to enable the game to count the number of user and computer wins, stop the game once someone had reached three and let the user know whether they had won or lost.
+For this, I implemented a basic 'while' loop within the 'play()' function which carried on looping until either the computer or user score reached 3, and also added code into the 'get winner()' function to respectively increase the values of the 'user_wins' and 'computer wins' variables. In order for this to work outside the local scope, it was important to refer both of the variables as 'global' within the 'get_winner()' function.
+
+![Alt text](image-8.png)
+
+At this stage, I encountered an issue where the webcam would automatically become inaccessible after the first loop iteration, I was able to overcome this by releasing the 'cap' object and shutting down the camera window after the 'prediction' variable had been assigned within the 'get_image_from_webcam()' function, rather than after the 'while' loop within the 'get_prediction()' function.
+
+![Alt text](image-9.png)
+
+The code works as expected!
+
+![Alt text](image-10.png)
+
+Finally, in order to tidy the functions into a single object, I packed them as methods into a class called RockPaperScissors. A few things needed to be modified here; after initializing the class and adding 'self' as an argument, I updated all of the methods with the 'self' argument and defined the class attributes as the image model as well as win scores for both the computer and user. 
+
+![Alt text](image-11.png)
+
+Making sure to add 'self' to the variables I had used as attributes, I also added execution code under an 'if name equals main' statement so that the code is only run when the 'camera_rps' file is run directly, and won't automatically run if the file is imported.
+
+![Alt text](image-12.png)
--- camera_rps.py
+++ camera_rps.py
@@ -1,112 +1,110 @@
 import time
 import os 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' #This line and the one above it disable irrelevant warning messages
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import cv2 
 from keras.models import load_model
 import numpy as np
-model = load_model('./keras_model.h5',compile=False)
-computer_wins = 0
-user_wins = 0
+import random
 
-def get_prediction():
-    print('Determining your choice...')
-    #countdown
-    t = 3
-    while t > 0:
-        print(f"Countdown: {t}")
-        t -= 1
-        time_start = time.time()
-        while (time.time() - time_start) < 1:
+class RockPaperScissors:
+    def __init__(self):
+        self.model = load_model('./keras_model.h5',compile=False)
+        self.computer_wins = 0
+        self.user_wins = 0
+
+    def get_prediction(self):
+        print('Determining your choice...')
+        #countdown
+        t = 3
+        while t > 0:
+            print(f"Countdown: {t}")
+            t -= 1
+            time_start = time.time()
+            while (time.time() - time_start) < 1:
+                pass
+
+        while True: 
+            prediction = self.get_image_from_webcam()
+            #Assign the correct probability to the correct gesture
+            if prediction[0][0] > 0.75:
+                x = 'Scissors'
+                break
+            
+            elif prediction[0][1] > 0.75:
+                x = 'Rock'
+                break
+
+            elif prediction[0][2] > 0.75:
+                x = 'Paper'
+                break
+            elif prediction[0][3] > 0.75:
+                x = 'None'
+        print(f"You chose {x}")
+        return x
+
+    def get_computer_choice(self):
+        selections = ['Rock', 'Paper', 'Scissors']
+        x = random.choice(selections)
+        return x
+
+    def get_winner(self, computer_choice, user_choice):
+        x = computer_choice
+        y = user_choice
+        
+        print(f'Your choice: {y}, computer choice {x}')
+        if x == y:
+            print("It is a tie!")
             pass
-
-    while True: 
-        prediction = get_image_from_webcam()
+        elif y == 'Rock' and x == 'Scissors':
+            print("You won")
+            self.user_wins += 1
+            pass
+        elif y == 'Scissors' and x == 'Paper':
+            print("You won")
+            self.user_wins += 1
+            pass
+        elif y == 'Paper' and x == 'Rock':
+            print("You won")
+            self.user_wins += 1
+            pass
+        elif x == 'Rock' and y == 'Scissors':
+            print("You lost")
+            self.computer_wins += 1
+            pass
+        elif x == 'Scissors' and y == 'Paper':
+            print("You lost")
+            self.computer_wins += 1
+            pass
+        elif x == 'Paper' and y == 'Rock':
+            print("You lost")
+            self.computer_wins += 1
+            pass
         
-        #Assign the correct probability to the correct gesture
-        if prediction[0][0] > 0.75:
-            x = 'Scissors'
-            break
+    def get_image_from_webcam(self):
+        cap = cv2.VideoCapture(0)
+        data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+        ret, frame = cap.read()
+        resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
+        image_np = np.array(resized_frame)
+        normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image
+        data[0] = normalized_image
+        cv2.imshow('frame', frame)
+        prediction = self.model.predict(data, verbose=0)
+
+        #release the cap object once the prediction variable is assigned
+        cap.release()
+        #Destroy all the windows
+        cv2.destroyAllWindows()
+        return prediction
+
+    def play(self):
         
-        elif prediction[0][1] > 0.75:
-            x = 'Rock'
-            break
+        while self.computer_wins < 3 and self.user_wins < 3:
+            self.get_winner(self.get_computer_choice(),self.get_prediction())
+        if self.user_wins > 2:
+            print("You win, well done")
+        if self.computer_wins > 2:
+            print("The computer beat you")
 
-        elif prediction[0][2] > 0.75:
-            x = 'Paper'
-            break
-        elif prediction[0][3] > 0.75:
-            x = 'None'
-    print(f"You chose {x}")
-    return x
-
-def get_computer_choice():
-    selections = ['Rock', 'Paper', 'Scissors']
-    import random
-    x = random.choice(selections)
-    return x
-
-def get_winner(computer_choice,user_choice):
-    global user_wins
-    global computer_wins
-    x = computer_choice
-    y = user_choice
-    
-    print(f'Your choice: {y}, computer choice {x}')
-    if x == y:
-        print("It is a tie!")
-        pass
-    elif y == 'Rock' and x == 'Scissors':
-        print("You won")
-        user_wins += 1
-        pass
-    elif y == 'Scissors' and x == 'Paper':
-        print("You won")
-        user_wins += 1
-        pass
-    elif y == 'Paper' and x == 'Rock':
-        print("You won")
-        user_wins += 1
-        pass
-    elif x == 'Rock' and y == 'Scissors':
-        print("You lost")
-        computer_wins += 1
-        pass
-    elif x == 'Scissors' and y == 'Paper':
-        print("You lost")
-        computer_wins += 1
-        pass
-    elif x == 'Paper' and y == 'Rock':
-        print("You lost")
-        computer_wins += 1
-        pass
-    
-def get_image_from_webcam():
-    cap = cv2.VideoCapture(0)
-    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-    ret, frame = cap.read()
-    resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
-    image_np = np.array(resized_frame)
-    normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image
-    data[0] = normalized_image
-    cv2.imshow('frame', frame)
-    prediction = model.predict(data, verbose=0)
-
-    #release the cap object once the prediction variable is assigned
-    cap.release()
-    # Destroy all the windows
-    cv2.destroyAllWindows()
-    return prediction
-
-
-    
-def play():
-    
-    while computer_wins < 3 and user_wins < 3:
-        get_winner(get_computer_choice(),get_prediction())
-    if user_wins > 2:
-        print("You win, well done")
-    if computer_wins > 2:
-        print("The computer beat you")
-    
-    
-play()
+if __name__ == "__main__":
+    RockPaperScissors().play()
